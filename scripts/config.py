da2e97f5f14d11ebc6277468a9ded80239203802
Stephen Wang <swang.2420@gmail.com>
2025-08-25 22:06:05 -0400

    pandas html read fix

--- scripts/config.py
+++ scripts/config.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timezone
 from pathlib import Path
 import pandas as pd
+import urllib.request
 
 def get_sp500_tickers():
 	url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-	tables = pd.read_html(url)
+	req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	tables = pd.read_html(urllib.request.urlopen(req), flavor="bs4")
 	sp500_tickers = tables[0]["Symbol"].str.replace(".", "-", regex=False).tolist()
 	return sp500_tickers
 
